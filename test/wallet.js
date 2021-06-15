17c083c532ed4596ea16a4f246f071fc32e0f234
Adam McMahon <mcmahonempire@gmail.com>
2021-06-15 17:37:49 -0400

    test approvers and quorum

--- test/wallet.js
+++ test/wallet.js
@@ -20,4 +20,14 @@ contract('Wallet', (accounts) => {
             }
         );
     });
+
+    it('should have correct approvers and quorum', async () => {
+        const approvers = await wallet.getApprovers();
+        const quorum = await wallet.quorum();
+        assert(approvers.length === 3);
+        assert(approvers[0] === accounts[0]);
+        assert(approvers[1] === accounts[1]);
+        assert(approvers[2] === accounts[2]);
+        assert(quorum.toNumber() === 2);
+    });
 });
