e3f768a79f370f1a8b63676ea1a4d37cd2806cb0
Adam McMahon <mcmahonempire@gmail.com>
2021-06-29 20:09:21 -0400

    add header containing quorum and accounts data

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,27 @@
 import React, { useEffect, useState } from "react";
 import { getWeb3, getWallet } from './utils.js';
+import Header from './Header.js';
 
 function App() {
   const [web3, setWeb3] = useState(undefined);
   const [accounts, setAccounts] = useState(undefined);
   const [wallet, setWallet] = useState(undefined);
+  const [approvers, setApprovers] = useState([]);
+  const [quorum, setQuorum] = useState(undefined);
 
   useEffect(() => {
     const init = async () => {
       const web3 = getWeb3();
       const accounts = await web3.eth.getAccounts();
       const wallet = await getWallet(web3);
+      const approvers = await wallet.methods.getApprovers().call();
+      const quorum = await wallet.methods.quorum().call();
 
       setWeb3(web3);
       setAccounts(accounts);
       setWallet(wallet);
+      setApprovers(approvers);
+      setQuorum(quorum);
     };
     init();
   }, []);
@@ -22,6 +29,8 @@ function App() {
   if ( typeof web3 === 'undefined' 
       || typeof accounts === 'undefined' 
       || typeof wallet === 'undefined' 
+      || typeof approvers === 'undefined'
+      || typeof quorum === 'undefined'
   ) {
     return <div>Loading...</div>;
   }
@@ -29,6 +38,7 @@ function App() {
   return (
     <div>
       Multisig Dapp
+      <Header approvers={approvers} quorum={quorum} />
     </div>
   );
 }
