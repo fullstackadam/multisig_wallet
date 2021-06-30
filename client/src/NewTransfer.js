d2ba150ec5f7a185b967f63ecb28aa65144dc51c
Adam McMahon <mcmahonempire@gmail.com>
2021-06-30 09:49:45 -0400

    add new transfer form

---
+++ client/src/NewTransfer.js
@@ -0,0 +1,39 @@
+import { set } from 'lodash';
+import React, { useState } from 'react';
+
+function NewTransfer({createTransfer}) {
+    const [transfer, setTransfer] = useState(undefined);
+
+    const submit = e => {
+        e.preventDefault();
+        createTransfer(transfer);
+    };
+
+    const updateTransfer = (e, field) => {
+        const value = e.target.value;
+        setTransfer({...transfer, [field]: value});
+    };
+
+    return (
+        <div>
+            <h2>Create transfer</h2>
+            <form onSubmit={() => submit(e)}>
+                <label htmlFor="amount">Amount</label>
+                <input 
+                    id="amount" 
+                    type="text" 
+                    onChange={e => updateTransfer(e, 'amount')}
+                />
+                <label htmlFor="to">To</label>
+                <input 
+                    id="to" 
+                    type="text" 
+                    onChange={e => updateTransfer(e, 'to')}
+                />
+                <button>Create</button>
+            </form>
+        </div>
+    );
+}
+
+export default NewTransfer;
